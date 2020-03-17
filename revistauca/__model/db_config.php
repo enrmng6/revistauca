08324742889907a5ada61d8dbca79a777b11a3a8
enrmng6 <61994979+enrmng6@users.noreply.github.com>
2020-03-16 23:37:48 -0600

    Add files via upload

---
+++ revistauca/__model/db_config.php
@@ -0,0 +1,24 @@
+<?php
+
+class MySQLConnection{
+	
+	private $conexion;
+	
+	public function __construct(){
+		$this->conexion = mysqli_connect('192.168.43.204', 'web1', 'web1', 'revistauca'); // server, user, passw, db
+		//$this->conexion = new mysqli('157.230.178.160', 'web1', 'web1', 'revistauca'); // server, user, passw, db
+		//$this->conexion = mysqli_connect('localhost', 'web1', 'web1', 'revistaucaapp');
+	}
+	
+	public function getResult($select_sql){
+		return $this->conexion->query($select_sql);
+		//return mysqli_query($this->conexion, $select_sql);
+	}
+	
+	public function executeStatement($sql){
+		return $this->conexion->query($sql);
+	}
+	
+}
+
+ ?>
