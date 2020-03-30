b691c414f00b28621b39686e3cb2e08f4b2f31e9
enrmng6 <61994979+enrmng6@users.noreply.github.com>
2020-03-30 01:12:25 -0500

    Add files via upload

--- revistauca/revistas/educar/detail.php
+++ revistauca/revistas/educar/detail.php
@@ -33,6 +33,7 @@ include $_SERVER["DOCUMENT_ROOT"] . "/revistauca/__view/shared/header.php";
 			<div class="inline">
 				<h4 id="educar_detail_date"><? echo $fila['creado']; ?></h4>
 				<h4 id="educar_detail_author">...</h4>
+				<h5 id="noticia_detail_author"><span id="numeroVisitas"></span></h5>
 			</div>
 			
 			<script>
@@ -126,7 +127,7 @@ include $_SERVER["DOCUMENT_ROOT"] . "/revistauca/__view/shared/header.php";
 </div> <!-- end #content -->
 
 <script>
-	registrarVisita(<? echo $_GET['educarid']; ?>);
+	registrarVisita(<? echo $_GET['educarid']; ?>, 'educar');
 </script>
 	
 <?php
