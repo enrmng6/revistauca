2f29ab793f50a994c66328923025ace407c8bda3
enrmng6 <61994979+enrmng6@users.noreply.github.com>
2020-03-30 23:41:44 -0600

    rutas controllers para arreglar lowercase controll

--- revistauca/admin/application/config/routes.php
+++ revistauca/admin/application/config/routes.php
@@ -52,3 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+$route['entrecontadores'] = "EntreContadores";
+$route['ucaprofesional'] = "UcaProfesional";
+/*$route['/(:any)'] = "/products/index/$1";*/
